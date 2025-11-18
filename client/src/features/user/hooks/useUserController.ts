fd97fc4b4d296c114dc529be6d47bb9a14042f27
Raystar247 <24star10@gmail.com>
2025-11-18 17:28:40 +0900

    ログイン時sign in 機構を簡易作成
    パスワード比較する機構をhandleSubmit関数内に実装
    将来的にreduxでログイン状態を管理するので、その時に移植予定

--- client/src/features/user/hooks/useUserController.ts
+++ client/src/features/user/hooks/useUserController.ts
@@ -1,9 +1,28 @@
 import type React from "react";
+import type { ApiResponse } from "../../../api/types/apiTypes";
+import type { User } from "../types";
+import { apiClient } from "../../../api/apiClient";
 
 export const useUserController = <T extends Record<string, any>>(form: T) => {
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     console.log('Form dubmitted with data:', form);
+    // ここにサインインロジックを追加
+    const result: ApiResponse<User[]> = await apiClient.get<User[]>('/user', {
+      params: { email: form.email }
+    });
+    if (result.error) {
+      console.error('Error fetching user:', result.error);
+      return;
+    }
+    console.log('fetched', result.data[0]);
+    if (form.password == result.data[0].password) {
+      console.log('Sign-in successful for user:', result.data[0]);
+    } else {
+      console.log('Invalid password');
+      console.log(`${result.data[0].password} vs ${form.password}`);
+    }
+
   };
   return { handleSubmit };
 };
